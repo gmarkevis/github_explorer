d5a44e2d3e1fbe8b8ad8f1e366b5cd3f48a9e8c2
Guilherme Pinheiro <guilhermemarkevis@hotmail.com>
2021-02-02 08:52:47 -0300

    Aplicação de listagem e consulta de repositórios

---
+++ routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import Dashboard from '../pages/DashBoard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repositories/:repository+" component={Repository} />
+  </Switch>
+)
+
+export default Routes;
